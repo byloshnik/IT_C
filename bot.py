91df576221722e9e98ca94094a934810ef1c4125
byloshnik <niki.fedin@gmail.com>
2021-07-17 11:35:05 +0300

    Допилил бота

--- bot.py
+++ bot.py
@@ -25,67 +25,74 @@ kb1 = InlineKeyboardMarkup(
         ]
     )
 
-
 @dp.message_handler(commands=['start'])
 async def process_start_command1(message: types.Message):
     await message.answer(f'{message.chat.username}, привет!\nГотов развиваться?', reply_markup=kb1)
 
-# @dp.callback_query_handler(text_contains="Да")
-# async def answer_yes(call: CallbackQuery):
-#     await types.answer(cache_time=60)
-#
-#     await types.message.answer("Молодец! Осталось только записаться на сайте http://127.0.0.1:5000/"
-#                               "\nНе забудь указать почту! (клик по кнопке)", reply_markup=kb_but1)
-
-@dp.message_handler(text_contains='Да')
-async def zapis(message: types.Message):
+@dp.callback_query_handler(text_contains="Да")
+async def answer_yes(call: CallbackQuery):
+    '''Заносим пользователя в БД'''
+    await call.answer(cache_time=60)
+    await call.message.answer("Молодец! Осталось только записаться на сайте http://127.0.0.1:5000/"
+                              "\nНе забудь указать почту! (клик по кнопке)", reply_markup=kb_but1)
+    user_id = call.from_user.id
+    user_name = call.from_user.username
     connect = sqlite3.connect('db_students.db')
     cursor = connect.cursor()
     connect.commit()
-    user_id = message.chat.id
-    user_name = message.chat.username
     cursor.execute(f'SELECT id FROM Students WHERE id = {user_id}')
     data = cursor.fetchone()
     connect.commit()
-    print(data)
-    print(user_id, user_name)
     if data is None:
-        cursor.execute("INSERT INTO Students VALUES(?,?,?);", (user_id,user_name, None))
+        cursor.execute("INSERT INTO Students VALUES(?,?,?,?);", (user_id,user_name, None, 1))
         connect.commit()
     else: print("Такой пользователь уже существует")
 
-# @dp.callback_query_handler(text_contains="Нет")
-# async def answer_yes(call: CallbackQuery):
-#     await call.answer(cache_time=60)
-#     await call.message.answer("Хм, а может стоит подробнее почитать информацию на сайте: http://127.0.0.1:5000/ ?")
-#
-# @dp.message_handler(commands=['deleteme'])
-# async def process_help_command(message: types.Message):
-#     connect = sqlite3.connect('db_students.db')
-#     cursor = connect.cursor()
-#     user_id = message.chat.id
-#     cursor.execute(f'DELETE FROM Students WHERE id = {user_id}')
-#     connect.commit()
+@dp.callback_query_handler(text_contains="Нет")
+async def answer_no(call: CallbackQuery):
+    '''Заносим пользователя в БД'''
+    await call.answer(cache_time=60)
+    await call.message.answer("Хм, а может стоит подробнее почитать информацию на сайте: http://127.0.0.1:5000/ ?")
+    user_id = call.from_user.id
+    user_name = call.from_user.username
+    connect = sqlite3.connect('db_students.db')
+    cursor = connect.cursor()
+    connect.commit()
+    cursor.execute(f'SELECT id FROM Students WHERE id = {user_id}')
+    data = cursor.fetchone()
+    connect.commit()
+    if data is None:
+        cursor.execute("INSERT INTO Students VALUES(?,?,?,?);", (user_id,user_name, None, 0))
+        connect.commit()
+    else: print("Такой пользователь уже существует")
 
+'''Стираем все в БД по пользователю'''
+@dp.message_handler(commands=['deleteme'])
+async def process_help_command(message: types.Message):
+    connect = sqlite3.connect('db_students.db')
+    cursor = connect.cursor()
+    user_id = message.chat.id
+    cursor.execute(f'DELETE FROM Students WHERE id = {user_id}')
+    connect.commit()
+'''Отправка почты с занесением в БД'''
 @dp.message_handler(text = 'Указать почту')
 async def process_email(msg: types.Message):
     await msg.answer(f'{msg.chat.username}, Введи почту: ')
     @dp.message_handler(content_types=['text'])
     async def handle_text(message: Message):
             user_email = f'{message.text}'
-            #print(user_email)
             connect = sqlite3.connect('db_students.db')
             cursor = connect.cursor()
             user_id = f'{message.chat.id}'
-            cursor.execute(f'SELECT rate FROM Students WHERE id = "{user_id}"')
-            rate = cursor.fetchone()
-            print(rate)
-            if rate[0] != user_email:
-                  cursor.execute(f'UPDATE Students SET rate = "{user_email}" WHERE id = "{user_id}"')
+            cursor.execute(f'SELECT email FROM Students WHERE id = "{user_id}"')
+            email = cursor.fetchone()
+            print(email)
+            if email[0] != user_email:
+                  cursor.execute(f'UPDATE Students SET email = "{user_email}" WHERE id = "{user_id}"')
                   connect.commit()
-                  await message.answer(f'{message.chat.username}, Ваша почта внесена в базу!')
-            elif rate[0] == user_email:
-                  await message.answer(f'{message.chat.username}, Эта почта уже используется')
+                  await message.answer(f'{message.chat.username}, Ваша почта внесена в базу!\nЖдите подтверждения на почту!')
+            elif email[0] == user_email:
+                  await message.answer(f'{message.chat.username}, Эта почта уже используется!\nПопробуйте другую!')
 
 if __name__ == '__main__':
     executor.start_polling(dp)
